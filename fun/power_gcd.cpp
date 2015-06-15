176a898ee925097386b0d5f98592f5f70eaac85f
Vedsar Kushwaha <vedsarkushwaha@gmail.com>
2015-06-15 17:48:17 +0530

    power commit

--- fun/power_gcd.cpp
+++ fun/power_gcd.cpp
@@ -1,5 +1,5 @@
-unsigned ll power(unsigned ll x,unsigned ll y) {
-	unsigned ll temp;
+ll power(ll x,ll y) {
+	ll temp;
 	if(y==0)
 		return 1;
 	temp=power(x, y/2);
@@ -8,11 +8,30 @@ unsigned ll power(unsigned ll x,unsigned ll y) {
 	else
 		return x*temp*temp;
 }
-unsigned ll gcd(unsigned ll a, unsigned ll b) {
+ll gcd(ll a, ll b) {
   while (b != 0)  {
-    int t = b;
+    ll t = b;
     b = a % b;
     a = t;
   }
   return a;
+}
+pair<int,pair<int,int> > egcd(ll a, ll b) {
+	if(a==0) return make_pair(b,make_pair(0,1));
+	else {
+		pair<int,pair<int,int> > temp;
+		temp=egcd(b%a,a);
+		ll g=temp.first;
+		ll y=temp.second.first;
+		ll x=temo.second.second;
+		return g,x-(b/a)*y,y;
+	}
+}
+ll modinv(a,m) {
+	temp=egcd(b%a,a);
+	ll g=temp.first;
+	ll y=temp.second.first;
+	ll x=temo.second.second;
+	if(g!=1) return null;
+	else return x%m;
 }
