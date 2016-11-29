57ad2d6755564aed6bcad93515defb46c6ba8e2f
Kushwaha <vedsark@f45c89be072b.ant.amazon.com>
2016-11-29 08:34:30 +0530

    Added 99.py

--- eulerproject/99.py
+++ eulerproject/99.py
@@ -1,4 +1,4 @@
-
+from math import log10
 
 if __name__=="__main__":
     lst=[]
@@ -14,14 +14,22 @@ if __name__=="__main__":
 
     mxNum = 0
     cnt=0
+    line = -1
     for i in lst:
         print i
         cnt+=1
         num,exp = i[0],i[1]
-        num = num ** (exp-mnExp+1)
+        # num = num ** (exp-mnExp+1)
+        num = exp * log10(num)
+        if cnt==516:
+            print len(str(num))
+        if cnt==709:
+            print len(str(num))
         if num>mxNum:
             print "Found Bigger Number"
             print cnt
             mxNum = num
+            line = cnt
         # print cnt
     # print mxNum
+    print line
